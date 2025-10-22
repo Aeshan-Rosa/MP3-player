80585e715a9cb49a516f4c91c38acfbdd345ec7f
Aeshan_Rosa <aeshaneruditharosa@gmail.com>
2025-10-23 02:54:06 +0530

    Add comment to script.js
    Added comment to script.js for clarity.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//please leave comments
+
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
